2ecdceb40103c93849f5d38bf1e9dc3b5849fbe5
Iwona Gozdziewska <iwona.gozdziewska@protonmail.com>
2020-06-07 19:33:35 +0200

    Add better run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-echo "Hello, "$USER". This script runs Docker container \
-after validating path to directory containig input files in your local machine, \
-which will be shared with Docker container."
-
-echo -n "Enter absolute path to directory containg input files to analysis (annotation, reference, sample, control):"
-read input_path
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+input_path="$SCRIPTPATH/input"
 
 
-if [ -d "$input_path" ]; then
-    :
-elif [ ! -d "$input_path" ]; then
-    echo "Given path to input_files does not exist."
-    exit 1
-elif [ "$input_path"=""]; then
-    echo "Path to input was not given, quitting."
+if [ -z "$(ls -A $input_path)" ]; then
+   echo "Input directory is empty. Place input files in directory: $input_path"
+else
+   echo "Not Empty"
 fi
 
 (sleep 2 && python -m webbrowser http://0.0.0.0:2000/index) &
 (sleep 5 && python -m webbrowser http://0.0.0.0:5000) &
 
-docker run -v $input_path:/app/input \
+docker run -v $SCRIPTPATH/input:/app/input \
 -v $SCRIPTPATH/config_files:/app/config_files \
 -v $SCRIPTPATH/output:/app/output \
 -p 2000:2000 -p 5000:5000 \
